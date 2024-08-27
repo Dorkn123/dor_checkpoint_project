025a3a3d556ab030b60ade63aa53d3c7c1763872
Dor Knafo <dorknafo@gmail.com>
2024-08-27 18:57:19 +0300

    try again from scratch now 0.7

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
+
   tags = {
     Name      = var.tag_name
     Owner     = var.tag_owner
@@ -10,13 +11,15 @@ resource "aws_s3_bucket" "this" {
     prevent_destroy = true
   }
 
+  # Check if the bucket already exists
   provisioner "local-exec" {
     when = create
     command = "echo 'Bucket created: ${self.bucket}'"
+    on_failure = continue
   }
-}
 
-data "aws_s3_bucket" "existing_bucket" {
-  bucket = aws_s3_bucket.this.bucket
-  depends_on = [aws_s3_bucket.this]
+  # If the bucket already exists, skip creation
+  depends_on = [
+    aws_s3_bucket.this
+  ]
 }
